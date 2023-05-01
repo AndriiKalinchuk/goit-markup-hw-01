d07a9f26907fda6ab411623a6428694561e41f7b
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-05-01 18:47:47 +0300

    summary

---
+++ js/start.js
@@ -0,0 +1,11 @@
+//let num = 1
+//num +=10
+//alert(num)
+const a = 5;
+const b = 10;
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a * b);
+//console.log(a / b);
+const resalt = a + b;
+console.log('resalt :>> ', resalt);
